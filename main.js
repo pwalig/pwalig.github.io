d324cf0785c3bbc8bd87cf36dce545fbe82b88d6
Paweł <pwalig03@gmail.com>
2025-05-17 13:10:39 +0200

    Make website responsive

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({ canvas });
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -14,9 +13,27 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-function animate() {
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
+
+function render() {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
     renderer.render( scene, camera );
 }
-renderer.setAnimationLoop( animate );
+
+renderer.setAnimationLoop( render );
